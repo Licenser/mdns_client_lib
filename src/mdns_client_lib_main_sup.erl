67b38d6fc2e8e305f86d6d6f76a5f4883a82682b
Heinz N. Gies <heinz@licenser.net>
2014-08-27 09:39:41 -0400

    tagged pooler.

--- src/mdns_client_lib_main_sup.erl
+++ src/mdns_client_lib_main_sup.erl
@@ -20,12 +20,12 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, {{one_for_one, 5, 10}, [{mdns_client_lib_sup, 
-				  {mdns_client_lib_sup, start_link, []}, 
-				  permanent, 5000, supervisor, [mdns_client_lib_sup]},
-				 {mdns_client_lib_connection_responder, 
-				  {gen_event, start_link, [{local, mdns_client_lib_connection_event}]},
-				  permanent, 5000, worker, []},
-				 {mdns_client_lib_call_fsm_sup, {mdns_client_lib_call_fsm_sup, start_link, []},
-				  permanent, 5000, supervisor, [mdns_client_lib_call_fsm_sup]}]}}.
-
+    {ok, {{one_for_one, 5, 10},
+          [{mdns_client_lib_sup,
+            {mdns_client_lib_sup, start_link, []},
+            permanent, 5000, supervisor, [mdns_client_lib_sup]},
+           {mdns_client_lib_connection_responder,
+            {gen_event, start_link, [{local, mdns_client_lib_connection_event}]},
+            permanent, 5000, worker, []},
+           {mdns_client_lib_call_fsm_sup, {mdns_client_lib_call_fsm_sup, start_link, []},
+            permanent, 5000, supervisor, [mdns_client_lib_call_fsm_sup]}]}}.
